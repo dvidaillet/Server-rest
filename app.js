d0d5f4c87753608dec18f0d383e2a0176cef11bb
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-06-15 21:12:32 -0400

    primeras configuraciones

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo')
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hola");
+});
+
+app.listen(port, () => {
+  console.log(`App corriendo en el puerto ${port}`);
+});
