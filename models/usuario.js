c4a57680406c3ff4376c68e8f9bff4823f399997
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-06-16 11:18:10 -0400

    Creando campos del modelo usuarios

---
+++ models/usuario.js
@@ -0,0 +1,40 @@
+const { model, Schema } = require("mongoose");
+
+const usuario = Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, "El nombre es obligatorio"],
+    },
+    email: {
+      type: String,
+      required: [true, "El correo es obligatorio"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "La contraseña es obligatoria"],
+    },
+    imagen: {
+      type: String,
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: ["ADMIN_ROLE", "USER_ROLE", "VENTAS_ROLE"],
+    },
+    estado: {
+      type: Boolean,
+      default: true,
+    },
+    google: {
+      type: Boolean,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = model("Usuario", usuario);
