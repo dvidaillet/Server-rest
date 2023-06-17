6d1e6df07e4cac8e4576cb7c991cc6147c8b79f3
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-06-16 23:03:16 -0400

    Validaciones de express en postUsuario

--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -6,9 +6,23 @@ const {
   postUsuario,
   putUsuario,
 } = require("../controller/usuarios.controller");
+const { check } = require("express-validator");
 
 userRouter.get("/", getUsuario);
-userRouter.post("/", postUsuario);
+userRouter.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").notEmpty(),
+    check("email", "El nombre es obligatorio").isEmail(),
+    check("password", "El nombre es obligatorio").isLength({ min: 6 }),
+    check("rol", "El rol no es valido").isIn([
+      "ADMIN_ROLE",
+      "USER_ROLE",
+      "VENTAS_ROLE",
+    ]),
+  ],
+  postUsuario
+);
 userRouter.put("/:id/:a", putUsuario);
 userRouter.delete("/", deleteUsuario);
 
