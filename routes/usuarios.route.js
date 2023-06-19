a3d8a6e85c73df1ee3a78e7ebae6a133d0d5e253
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-06-19 08:52:33 -0400

    creando validar campos middleware

--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -7,19 +7,23 @@ const {
   putUsuario,
 } = require("../controller/usuarios.controller");
 const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 userRouter.get("/", getUsuario);
 userRouter.post(
   "/",
   [
     check("nombre", "El nombre es obligatorio").notEmpty(),
-    check("email", "El nombre es obligatorio").isEmail(),
-    check("password", "El nombre es obligatorio").isLength({ min: 6 }),
+    check("email", "El Formato del email es incorrecto").isEmail(),
+    check("password", "La password contiene menos de 6 caracteres").isLength({
+      min: 6,
+    }),
     check("rol", "El rol no es valido").isIn([
       "ADMIN_ROLE",
       "USER_ROLE",
       "VENTAS_ROLE",
     ]),
+    validarCampos,
   ],
   postUsuario
 );
