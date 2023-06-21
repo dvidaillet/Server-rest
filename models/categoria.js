0e005cc62262954e007ebf18fac6a957566eeada
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-06-20 22:21:15 -0400

    Modelo de Categorias terminado

--- models/categoria.js
+++ models/categoria.js
@@ -1,5 +1,20 @@
 const { model, Schema } = require("mongoose");
 
-const categoriaSchema = new Schema({});
+const categoriaSchema = new Schema({
+  nombre: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+    required: [true, "El estado es obligatorio"],
+  },
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: "Usuario",
+    required: true,
+  },
+});
 
 module.exports = model("Categoria", categoriaSchema);
